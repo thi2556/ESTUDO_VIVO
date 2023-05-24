8a909ed368e3a6531bb17ee6cff14e543883723b
Diretorio_TH0 <83072024+thi2556@users.noreply.github.com>
2023-05-24 23:17:50 +0000

    update

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const port = 3000
 app.get('/', async (req, res) => {
     var connection = await mysql.createConnection({
         host: "sql9.freemysqlhosting.net",
-        user: "sql9620111",
-        password: "uAaitpTeM3",
-        database: 'sql9620111'
+        user: "sql9620952",
+        password: "JlGvzTsmJm",
+        database: 'sql9620952'
     })
 
-    const [rows] = await connection.execute('SELECT * FROM clusters_co')
+    const [rows] = await connection.execute('SELECT * FROM usuarios')
 
     connection.end();
     res.send(rows)
